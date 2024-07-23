b6e97dbd8b00b8f524e378fbb224394ee471d8b5
David Fuller <davestoneyfuller@gmail.com>
2024-07-23 11:52:15 -0400

    updates to logic
    use external refresh function, and add main logic

--- config.py
+++ config.py
@@ -2,12 +2,12 @@
 
 #   Cloud Config
 cloud_url = 'https://davefsandbox.us.qlikcloud.com/'
-api_key = 'JzdWJUeXBlIjoidXNlciIsInRlbmFudElkIjoiZTN4YXBWLThyMUJmektmOUVERFFrcUt2dWE0djhHbTgiLCJqdGkiOiI0MDY4NDQ2Yi01M2NjLTQzMzYtYjg1MS00N2NhYzUzZDEwMjUiLCJhdWQiOiJxbGlrLmFwaSIsImlzcyI6InFsaWsuYXBpL2FwaS1rZXlzIiwic3ViIjoiMEdEZTduSElRREp1UmY4X25EQWJFZkozUEtoNHJXQnkifQ.r7g_20OWzuC9T_tczmwT9Gsu2kfyzQldv_xi-1z90CU3KzxQsdyYJXloIiDyov2QQ3vLUWlGOZQ-Hvy3BA5QCJu8MEWc1oVCA5RD0Tp78iMcIjM37iPOhXNHFh3oQ87z'
+api_key = 'eyJhbGciOiJFUzM4NCIsImtpZCI6IjFkOWQ4ZDMxLTNlNDctNDY3Yi1iNjk0LWY4MmEyNTZmNDYyZCIsInR5cCI6IkpXVCJ9.eyJzdWJUeXBlIjoidXNlciIsInRlbmFudElkIjoiZTN4YXBWLThyMUJmektmOUVERFFrcUt2dWE0djhHbTgiLCJqdGkiOiIxZDlkOGQzMS0zZTQ3LTQ2N2ItYjY5NC1mODJhMjU2ZjQ2MmQiLCJhdWQiOiJxbGlrLmFwaSIsImlzcyI6InFsaWsuYXBpL2FwaS1rZXlzIiwic3ViIjoiMEdEZTduSElRREp1UmY4X25EQWJFZkozUEtoNHJXQnkifQ.tr8TzxyG3ILsI5R-OUhFChuDUNpTxFCx7vuMGBkOz6bt7vAu4v37XuyG-Nfp827dWwPjBTC_Hvl7OlZ3pW4yItmjgGDXMbKdKGPO_-7p_89--oKoaS4bY7ferl1z0wU5'
 appId = '3bb21df4-d184-48b3-a22b-e33e65301596'
 
 #On Prem Config
 node = "https://qseowcentral"
-cert = 'C:\qscerts\clientandkey.pem'
+cert = "C:\qscerts\clientandkey.pem"
 user = "UserDirectory=test;UserId=svc_acct"
 taskId = "bf8e0ed9-21c5-47e8-a8a2-18f601e79546"
 
--- main.py
+++ main.py
@@ -1,44 +1,29 @@
-import time
-import json
-import requests
 import config
 import pandas as pd
+import qseowRefresh
 from sqlalchemy import create_engine
+from datetime import date
 
-#reload the app specified in config
-def refresh_data():
-    url = config.cloud_url + "api/v1/reloads"
-    headers = {
-        'Authorization': 'Bearer ' + config.api_key,
-        'Content-type': 'application/json'
-    }
-    payload = json.dumps({
-        "appId": config.appId,
-        "partial": False
-    })
-    response = requests.request("POST", url, headers=headers, data=payload)
-#    print(url)
-#    print(headers)
-#    print(payload)
-    print(response.text)
+today = (date.today())
+lastrefresh = ""
 
 
-#Check the Hive database for a True value
-def isItDone():
-    while True:
-        conn = f'hive://{config.user_name}:{config.passwd}@{config.host_server}:{config.port}/{config.database}'
-        engine = create_engine(conn, connect_args={'auth': 'LDAP'})
+# Check the Hive database for a True value
+def isitdone():
+    conn = f'hive://{config.user_name}:{config.passwd}@{config.host_server}:{config.port}/{config.database}'
+    engine = create_engine(conn, connect_args={'auth': 'LDAP'})
 
-        query = "select case when ar.ts>br.ts then true else false end as status from ea_common.channel_hive_table_info_br br INNER JOIN ea_common.channel_hive_table_info_ar ar on br.tablename=ar.tablename where ar.tablename='chnlptnr_sellin_fact';"
-        data = pd.read_sql(query, con=engine)
-        print(data)
-        return data
+    query = "select case when ar.ts>br.ts then true else false end as status from ea_common.channel_hive_table_info_br br INNER JOIN ea_common.channel_hive_table_info_ar ar on br.tablename=ar.tablename where ar.tablename='chnlptnr_sellin_fact';"
+    data = pd.read_sql(query, con=engine)
+    print(data)
+    global lastrefresh
+    lastrefresh = date.today()
 
+# main function
+def main():
+    global lastrefresh
+    while lastrefresh != today:
+        isitdone()
+    qseowRefresh.refresh_data()
 
-#def main():
-
-
-
-
-
-refresh_data()
+main()
