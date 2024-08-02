d2e56ab936c298e30e666ebe04c0d353fefadbf4
David Fuller <davestoneyfuller@gmail.com>
2024-08-02 15:14:56 -0400

    Update prestopyhive.py

--- prestopyhive.py
+++ prestopyhive.py
@@ -14,5 +14,5 @@ print(data)
 # engine = create_engine(conn, connect_args={'auth': 'LDAP'})
 #    query = "select case when ar.ts>br.ts then true else false end as status from ea_common.channel_hive_table_info_br br INNER JOIN ea_common.channel_hive_table_info_ar ar on br.tablename=ar.tablename where ar.tablename='chnlptnr_sellin_fact';"
 #    data = pd.read_sql(query, con=engine)
-     print(data)
+#    print(data)
 #    return data
