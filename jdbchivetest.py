263ec56f69fe4ef96e7225b37e85f01f6ae5d4b5
David Fuller <davestoneyfuller@gmail.com>
2024-08-02 14:37:33 -0400

    updated some values, changed query

--- jdbchivetest.py
+++ jdbchivetest.py
@@ -1,9 +1,10 @@
 import jaydebeapi
+import config
 
 # Connection parameters
 jdbc_url = 'jdbc:hive2://knox.host:8443/default'  # JDBC URL for HiveServer2
-username = 'your-username'
-password = 'your-password'
+username = config.user_name
+password = config.passwd
 jar_file = '/path/to/hive-jdbc-driver.jar'  # Path to the Hive JDBC driver JAR file
 
 # Establish connection to Hive
@@ -18,7 +19,7 @@ conn = jaydebeapi.connect(
 cursor = conn.cursor()
 
 # Execute Hive query
-cursor.execute('SELECT * FROM hive_table')
+cursor.execute('show tables;')
 
 # Fetch results
 result = cursor.fetchall()
