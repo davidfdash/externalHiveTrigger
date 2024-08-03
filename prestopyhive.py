1700e3aac8d14384fa5fcf62f227c350814ab3b0
David Fuller <davestoneyfuller@gmail.com>
2024-08-03 12:34:39 -0400

    updates for presto

--- prestopyhive.py
+++ prestopyhive.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 import pandas as pd
 import config
 
-Presto_conn = presto.connect(host=config.host_server,
+Presto_Conn = presto.connect(host=config.host_server,
                              port=8014,
                              protocol='https',
                              requests_kwargs={'verify':False,'auth':(config.user_name, config.passwd)})
@@ -11,8 +11,12 @@ final_query = "select case when ar.ts>br.ts then true else false end as status f
 data = pd.read_sql(final_query, con=Presto_Conn)
 print(data)
 
+Presto_Conn.close()
 # engine = create_engine(conn, connect_args={'auth': 'LDAP'})
 #    query = "select case when ar.ts>br.ts then true else false end as status from ea_common.channel_hive_table_info_br br INNER JOIN ea_common.channel_hive_table_info_ar ar on br.tablename=ar.tablename where ar.tablename='chnlptnr_sellin_fact';"
 #    data = pd.read_sql(query, con=engine)
 #    print(data)
 #    return data
+script =f"""select case when ar.ts>br.ts then true else false end as status from ea_common.channel_hive_table_info_br br INNER JOIN ea_common.channel_hive_table_info_ar ar on br.tablename=ar.tablename where ar.tablename='chnlptnr_sellin_fact'; """
+
+script =f"""select * from table limit 10"""
